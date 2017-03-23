7313a21448f3a0036752689f070a4a91a5cc032b
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-23 21:45:28 +0100

    Add npm package parse and import it: ERROR in D:/tangu/Documents/Projects/cannot-find-parse/src/app/app.component.ts (2,21): Cannot find module 'parse'.)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Parse} from 'parse';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    this.initParse();
+  }
+
+  private initParse() {
+    Parse.initialize("app id");
+    Parse.serverURL = "app url";
+  }
 }
