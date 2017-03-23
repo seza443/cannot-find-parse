460c7341b714d6b934d75cbb37cf6433f11324a2
Tanguy Vaessen <tanguy.vaessen@gmail.com>
2017-03-23 21:49:38 +0100

    src\declarations.d.ts solves the problem

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+/*
+  Declaration files are how the Typescript compiler knows about the type information(or shape) of an object.
+ They're what make intellisense work and make Typescript know all about your code.
+
+ A wildcard module is declared below to allow third party libraries to be used in an app even if they don't
+ provide their own type declarations.
+ source: Ionic 2 app
+*/
+declare module '*';
